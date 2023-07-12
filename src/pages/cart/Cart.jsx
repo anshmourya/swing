a79b2e207c62d0c68c88daac18a9d21e75c5b11a
anshmourya <anshmourya657@gmail.com>
2023-07-12 16:42:14 +0530

    feature: added cart page

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,5 +1,67 @@
+import { useContext } from "react";
+import { CartData } from "../../hooks/Cart";
+import ProductCard from "../../components/generalCoponents/card/ProductCard";
+import Nav from "../../components/generalCoponents/navBar/Nav";
 const Cart = () => {
-  return <div>Cart</div>;
+  const { cartItems, cartItemsData } = useContext(CartData);
+
+  return (
+    <>
+      <Nav />
+      {/* {!---------!} */}
+      <div className="container flex items-start justify-center m-auto max-lg:flex-col">
+        {/* {!---------!} */}
+
+        <div className="lg:w-[70%]">
+          {cartItems &&
+            cartItems.map(
+              (item) =>
+                item.cartItems !== 0 && (
+                  <ProductCard
+                    key={item.id}
+                    title={item.title}
+                    image={item.image}
+                    price={item.price}
+                    id={item.id}
+                    wholeData={item}
+                  />
+                )
+            )}
+        </div>
+        {/* {!---------!} */}
+
+        <SubTotal SubTotalData={cartItemsData} />
+      </div>
+    </>
+  );
 };
 
 export default Cart;
+
+function SubTotal({ SubTotalData }) {
+  const { items, price } = SubTotalData || 0;
+  return (
+    <div className="flex-1 w-full p-6 mt-6 bg-white border rounded-lg shadow-md">
+      <div className="flex justify-between mb-2">
+        <p className="text-gray-700">Total Item</p>
+        <p className="text-gray-700">{items}</p>
+      </div>
+      <div className="flex justify-between">
+        <p className="text-gray-700">Subtotal</p>
+        <p className="text-gray-700"> &#8377; {price}</p>
+      </div>
+      <hr className="my-4" />
+      <div className="flex justify-between">
+        <p className="text-lg font-bold">Total</p>
+        <div className="">
+          <p className="mb-1 text-lg font-bold">&#8377; {price} INR</p>
+          <p className="text-sm text-gray-700">including VAT</p>
+        </div>
+      </div>
+
+      <button className="mt-6 w-full rounded-md bg-orange-400 py-1.5 font-medium text-white hover:shadow-md">
+        Check out
+      </button>
+    </div>
+  );
+}
