49633b72627771f1d3b861d6f89697a2637de664
anshmourya <anshmourya657@gmail.com>
2023-07-26 20:20:51 +0530

    fixed the cors bug

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors({
     origin: process.env.CLIENT_URl
 }))
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Origin', process.env.CLIENT_URl);
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true'); // Set the header to 'true'
